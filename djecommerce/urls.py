a97e233d03c67c4cd0062761e10364088c0a83c2
AnvarKhan <45309516+AnvarKhan@users.noreply.github.com>
2021-12-26 18:04:14 +0500

    e-commerce

--- djecommerce/urls.py
+++ djecommerce/urls.py
@@ -10,7 +10,9 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL,
-                          document_root=settings.STATIC_ROOT)
+    import debug_toolbar
+    urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
     urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
